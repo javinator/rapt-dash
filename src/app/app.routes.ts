2db63f2be061e42a506fba11c590547bfa4b1f4f
Vince Hofer <mail@vincehofer.ch>
2026-01-27 17:59:49 +0100

    redirect unknown routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,16 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadComponent: () => import('./pages/home/home.page').then((m) => m.HomePage),
+    loadComponent: () =>
+      import('./pages/home/home.page').then((m) => m.HomePage),
   },
   {
     path: 'dash',
-    loadComponent: () => import('./pages/dashboard/dashboard.page').then((m) => m.DashboardPage),
+    loadComponent: () =>
+      import('./pages/dashboard/dashboard.page').then((m) => m.DashboardPage),
+  },
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
